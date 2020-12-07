d5d921d80e495ed0d3a0e92ff565b28580bb8b5f
Hasan <hasanjacob@yahoo.com>
2020-12-07 20:14:25 +1100

    Random quote bot works now

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,17 @@
+import random
+
 def entry_point_method():
   print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+
+  last = len(quotes) - 1
+  #randint both endpoints are inclusive
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   entry_point_method()
